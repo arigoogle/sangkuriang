bdb19b380677f7b4e558e00b59ca50cadc08a9ee
arigoogle <ariyantomail@gmail.com>
2022-07-07 17:09:02 +0700

    remove js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -937,26 +937,4 @@
 
     
 })(jQuery);
-// function to set a given theme/color-scheme
-function setTheme(themeName) {
-    localStorage.setItem('mala_theme', themeName);
-    document.documentElement.className = themeName;
-}
-// function to toggle between light and dark theme
-function toggleTheme() {
-    if (localStorage.getItem('mala_theme') === 'theme-dark') {
-        setTheme('theme-light');
-    } else {
-        setTheme('theme-dark');
-    }
-}
-// Immediately invoked function to set the theme on initial load
-(function () {
-    if (localStorage.getItem('mala_theme') === 'theme-dark') {
-        setTheme('theme-dark');
-        document.getElementById('slider').checked = false;
-    } else {
-        setTheme('theme-light');
-      document.getElementById('slider').checked = true;
-    }
-})();
+
